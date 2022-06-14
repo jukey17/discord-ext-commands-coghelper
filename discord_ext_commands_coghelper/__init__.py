2ad6223f3934e2220fafa766180d0fae72f0a68f
jukey17 <keykun.pv@gmail.com>
2022-06-14 20:01:46 +0900

    bump v0.0.5

--- discord_ext_commands_coghelper/__init__.py
+++ discord_ext_commands_coghelper/__init__.py
@@ -3,6 +3,6 @@ from .utils import *
 from .coghelper import *
 
 __title__ = "discord_ext_commands_coghelper"
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 __author__ = "jukey17"
 __licence__ = "MIT"
